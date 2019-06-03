a773669f697bb1cef23b80683151bc8e6d66ccb2
bhagh <haghb1@gmail.com>
2019-06-03 16:17:52 -0400

    Initial submission

---
+++ house.py
@@ -0,0 +1,28 @@
+annual_salary = input("Enter your annual salary: ")
+portion_saved = input("Enter the percent of your salary to save, as a decimal: ")
+total_cost = input("Enter the cost of your dream home: ")
+
+portion_down_payment = float(total_cost) * float(.25)
+monthly_income = float(annual_salary) / 12
+monthly_investment = float(monthly_income) * float(portion_saved)
+
+current_savings = 0
+months = 0
+r = .04
+
+#while current_savings <= portion_down_payment:
+    #current_savings = (current_savings + monthly_investment) + ((current_savings + monthly_investment) * (r / 12))
+    #months = months + 1
+#print("Number of months: ", months)
+
+while current_savings < portion_down_payment:
+    months += 1
+    current_savings +=  monthly_investment
+    interest = current_savings * r / 12
+    current_savings += interest
+print("Number of months: ", months)
+
+
+
+
+
